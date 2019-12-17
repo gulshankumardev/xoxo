ee15ecb7d1b3a9b8f4f4441a06ff53bb1139cf23
Gulshan Kumar <kumar.gulshan80@gmail.com>
2019-12-18 02:22:22 +0530

    feat(prisma): prisma client integration

--- server/resolvers/query.js
+++ server/resolvers/query.js
@@ -9,6 +9,12 @@ const query = {
       updatedAt: new Date(),
     };
   },
+
+  users: async (parent, args, ctx) => {
+    const userInfo = ctx.prisma.users();
+
+    return userInfo;
+  },
 };
 
 module.exports = { query };
