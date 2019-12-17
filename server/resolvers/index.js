427cb7f14becface3a8e31efc27a7e334615ef7d
Gulshan Kumar <kumar.gulshan80@gmail.com>
2019-12-18 02:40:22 +0530

    feat(prisma): datamodel relation-resolver methods

--- server/resolvers/index.js
+++ server/resolvers/index.js
@@ -1,7 +1,14 @@
+const { prisma } = require('../prisma/generated-client');
 const { query: Query } = require('./query');
 
 const resolvers = {
   Query,
+  User: {
+    posts: parent => prisma.user({ id: parent.id }).posts(),
+  },
+  Post: {
+    author: parent => prisma.post({ id: parent.id }).author(),
+  },
 };
 
 module.exports = resolvers;
