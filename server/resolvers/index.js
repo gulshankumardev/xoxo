bd728fc7b5cd0789db42232a001bf3a312abf8f0
Gulshan Kumar <kumar.gulshan80@gmail.com>
2019-12-22 21:31:40 +0530

    feat(mutations): mutation schema and resolver
    * login/signup resolvers with prisma client integration

--- server/resolvers/index.js
+++ server/resolvers/index.js
@@ -1,13 +1,17 @@
 const { prisma } = require('../prisma/generated-client');
 const { query: Query } = require('./query');
+const { mutation: Mutation } = require('./mutation');
 
 const resolvers = {
   Query,
+  Mutation,
   User: {
     posts: parent => prisma.user({ id: parent.id }).posts(),
+    likedPosts: parent => prisma.user({ id: parent.id }).likedPosts(),
   },
   Post: {
     author: parent => prisma.post({ id: parent.id }).author(),
+    likedBy: parent => prisma.post({ id: parent.id }).likedBy(),
   },
 };
 
